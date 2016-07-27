bc9249f1f0b3961aaad81a9897c18c51ea27eaba
Nikki <nicleemart@gmail.com>
2016-07-27 15:48:50 -0500

    made some progress updating values

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// JavaScript
+window.addEventListener("load", function(){
+
+	var apiRequest = new XMLHttpRequest();
+
+		apiRequest.addEventListener("load", function(e){
+
+			var apiResponse = JSON.parse(e.target.responseText);
+			debugger;
+			var currentSummary = document.getElementById("currentSummary");
+			var currentTemp = document.getElementById("currentTemp");
+			var todayHigh = document.getElementById("todayHigh");
+			var todayLow = document.getElementById("todayLow");
+			var dailySummary = document.getElementById("dailySummary");
+			var rainChance = document.getElementById("rainChance");
+			var humidity = document.getElementById("humidity");
+
+			currentSummary.innerHTML = apiResponse.currently.summary;
+			currentTemp.innerHTML = apiResponse.currently.apparentTemperature;
+			todayHigh.innerHTML = apiResponse.daily.data[0].apparentTemperatureMax;
+			todayLow.innerHTML = apiResponse.daily.data[0].apparentTemperatureMin;
+			dailySummary.innerHTML = apiResponse.daily.summary;
+			rainChance.innerHTML = apiResponse.daily.data[0].precipProbability + "%";
+			humidity.innerHTML = apiResponse.daily.data[0].humidity + "%";
+
+		});
+
+    apiRequest.open("GET", "api");
+    apiRequest.send();
+
+});
