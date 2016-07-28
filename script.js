cddc269b4f2301b03ea627ddef23029ca2dec92d
Nikki <nicleemart@gmail.com>
2016-07-27 19:49:33 -0500

    added conversions

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function(){
 		apiRequest.addEventListener("load", function(e){
 
 			var apiResponse = JSON.parse(e.target.responseText);
-			debugger;
+		debugger;
 			var currentSummary = document.getElementById("currentSummary");
 			var currentTemp = document.getElementById("currentTemp");
 			var todayHigh = document.getElementById("todayHigh");
@@ -22,17 +22,17 @@ window.addEventListener("load", function(){
 			var visibility = document.getElementById("visibility");
 
 			currentSummary.innerHTML = apiResponse.currently.summary;
-			currentTemp.innerHTML = apiResponse.currently.apparentTemperature + "&deg;";
-			todayHigh.innerHTML = apiResponse.daily.data[0].temperatureMax;
-			todayLow.innerHTML = apiResponse.daily.data[0].temperatureMin;
+			currentTemp.innerHTML = Math.round(apiResponse.currently.apparentTemperature) + "&deg;";
+			todayHigh.innerHTML = Math.round(apiResponse.daily.data[0].temperatureMax) + "&deg;";
+			todayLow.innerHTML = Math.round(apiResponse.daily.data[0].temperatureMin) + "&deg;";
 			dailySummary.innerHTML = apiResponse.daily.summary;
-			sunrise.innerHTML = apiResponse.daily.data[0].sunriseTime;
-			sunset.innerHTML = apiResponse.daily.data[0].sunsetTime;
+			sunrise.innerHTML = convertTime(apiResponse.daily.data[0].sunriseTime);
+			sunset.innerHTML = convertTime(apiResponse.daily.data[0].sunsetTime);
 			rainChance.innerHTML = apiResponse.daily.data[0].precipProbability + "%";
-			humidity.innerHTML = apiResponse.daily.data[0].humidity + "%";
+			humidity.innerHTML = apiResponse.daily.data[0].humidity * 100 + "%";
 			wind.innerHTML = apiResponse.daily.data[0].windSpeed;
-			feelsLike.innerHTML = apiResponse.daily.data[0].apparentTemperatureMax + "&deg;";
-			precipAmount.innerHTML = apiResponse.daily.data[0].precipIntensity;
+			feelsLike.innerHTML = Math.round(apiResponse.daily.data[0].apparentTemperatureMax) + "&deg;";
+			precipAmount.innerHTML = apiResponse.daily.data[0].precipIntensity + " in";
 			pressure.innerHTML = apiResponse.daily.data[0].pressure;
 			visibility.innerHTML = apiResponse.daily.data[0].visibility + " mi";
 
@@ -40,5 +40,12 @@ window.addEventListener("load", function(){
 
     apiRequest.open("GET", "api");
     apiRequest.send();
+
+    function convertTime (unixTime){
+    	var date = new Date(unixTime*1000);
+		var hours = date.getHours();
+		var minutes = "0" + date.getMinutes();
+		return hours + ':' + minutes.substr(-2);
+    }
 
 });
